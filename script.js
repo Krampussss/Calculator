b08309cad3066bd6c10728e12ef67dbd055f50ea
Krampussss <95955435+Krampussss@users.noreply.github.com>
2023-12-30 01:29:45 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const display = document.getElementById("display");
+let flag = 0,ans=0;
+
+function cls()
+{
+    display.value = "";
+    flag = 0;
+}
+
+function disp(val)
+{
+    display.value = display.value + val;
+    if(flag == 1)
+    {
+
+            display.value = "";
+            flag = 0;
+            display.value = display.value + val;
+    }
+}
+
+function solve()
+{
+    try 
+    {
+        ans = eval(display.value);
+        display.value = ans;
+        flag = 1;
+    } 
+    catch (error) 
+    {
+        display.value = "Error";
+    }
+}
+
+
